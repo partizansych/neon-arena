c96b1babcd6cca90983b5171c3c65daf290cf6c2
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-03-26 11:08:21 +0700

    Обновить scene-loader

--- scripts/core/scene_loader.gd
+++ scripts/core/scene_loader.gd
@@ -5,55 +5,51 @@ extends Node
 @export var loading_screen_scene: PackedScene
 @export var use_sub_threads: bool = true
 
-## Внутреннее состояние
-var _current_load_path: String = ""
-var _loading_screen: LoadingScreen = null
 var _is_loading: bool = false
+var _loading_screen: LoadingScreen = null
 
 func change_scene_async(path: String) -> void:
 	if _is_loading:
 		push_warning("SceneLoader: Already loading a scene. Request ignored.")
 		return
-
-	_current_load_path = path
+	
 	_is_loading = true
 	await _show_loading_screen()
 
-	var err := ResourceLoader.load_threaded_request(path, "", use_sub_threads)
-	if err != OK:
-		_hide_loading_screen()
-		_reset_state()
-		push_error("SceneLoader: Failed to start threaded load: %s" % error_string(err))
+	var error: Error = ResourceLoader.load_threaded_request(path, "", use_sub_threads)
+	if error != OK:
+		push_error("SceneLoader: Failed to start threaded load: %s" % error_string(error))
+		_reset()
 		return
-	set_process(true)
 
-## Внутренние методы
+	var progress: Array[float] = [0.0]
+	while _is_loading:
+		var status := ResourceLoader.load_threaded_get_status(path, progress)
+		match status:
+			ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+				push_error("SceneLoader: Invalid resource: %s" % path)
+				await _reset()
+			ResourceLoader.THREAD_LOAD_FAILED:
+				push_error("SceneLoader: Failed: %s" % path)
+				await _reset()
+			ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+				await get_tree().process_frame
+			ResourceLoader.THREAD_LOAD_LOADED:
+				var resource := ResourceLoader.load_threaded_get(path)
+				get_tree().change_scene_to_packed(resource)
+				await _reset()
+
+func _reset() -> void:
+	_is_loading = false
+	if _loading_screen != null:
+		await _hide_loading_screen()
+
 func _show_loading_screen() -> void:
 	_loading_screen = loading_screen_scene.instantiate()
 	add_child(_loading_screen)
-	await _loading_screen.fade_in_finished
+	await _loading_screen.fade_in()
 
 func _hide_loading_screen() -> void:
 	await _loading_screen.fade_out()
 	_loading_screen.queue_free()
 	_loading_screen = null
-
-func _reset_state() -> void:
-	_is_loading = false
-	_current_load_path = ""
-	set_process(false)
-
-func _ready():
-	_reset_state()
-
-func _process(_delta: float) -> void:
-	var progress: Array
-	var status := ResourceLoader.load_threaded_get_status(_current_load_path, progress)
-	print("s")
-
-	match status:
-		ResourceLoader.THREAD_LOAD_LOADED:
-			var resource := ResourceLoader.load_threaded_get(_current_load_path)
-			get_tree().change_scene_to_packed(resource)
-			_reset_state()
-			_hide_loading_screen()
--- scripts/ui/loading_screen.gd
+++ scripts/ui/loading_screen.gd
@@ -1,23 +1,12 @@
 class_name LoadingScreen extends CanvasLayer
 
-signal fade_in_finished()
-signal fade_out_finished()
-
 @export var animation_player: AnimationPlayer
-@export var progress_bar: ProgressBar
-@export var status_label: Label
-@export var scene_name_label: Label
 
 func fade_in() -> void:
 	animation_player.play("transition")
 	await animation_player.animation_finished
-	fade_in_finished.emit()
 
 func fade_out() -> void:
 	animation_player.play_backwards("transition")
 	await animation_player.animation_finished
-	fade_out_finished.emit()
 	queue_free()
-
-func _ready() -> void:
-	fade_in()
