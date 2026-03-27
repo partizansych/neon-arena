045b4a4d9a0971340d00a6205f6cc7b6d974d544
Maxim Chaptykov <maks.chaptykov@gmail.com>
2026-03-27 07:38:40 +0700

    Добавить механику здоровья

---
+++ scripts/gameplay/health_component.gd
@@ -0,0 +1,24 @@
+class_name HealthComponent extends Node
+
+signal changed(old: float, new: float)
+signal died()
+
+@export var max_health: float = 100.0
+@export var is_invulnerable: bool = false
+
+var current_health: float:
+	set(value):
+		value = clampf(value, 0.0, max_health)
+		if value == current_health:return
+		if value == 0.0:
+			die()
+			return
+		var old: float = current_health
+		current_health = value
+		changed.emit(old, current_health)
+
+func die() -> void:
+	died.emit()
+
+func _ready() -> void:
+	current_health = max_health
